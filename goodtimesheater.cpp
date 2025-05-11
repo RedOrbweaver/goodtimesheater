b445f5d3dd1b9f144af85c3e9c7f6a1a4feac3d2
RedOrbweaver <oliwier.ciesielski@proton.me>
2025-05-11 19:18:59 +0200

    kicad files

--- goodtimesheater.cpp
+++ goodtimesheater.cpp
@@ -17,42 +17,39 @@ int main()
     printf("starting goodtimesheater...\n");
 
     StartupLEDEffect();
-
     
-
-    sleep_ms(10);
     ircam = std::make_unique<MLX90614>(I2C_PORT, I2C_SDA, I2C_SCL);
     sleep_ms(10);
-    printf("123\n");
-    printf("emiss: %f\n", ircam->GetEmissivity());
-    ircam->SetEmissivity(0.5);
-    printf("emisschange: %f\n", ircam->GetEmissivity());
-    while(true)
-    {
-        MLX90614::sensor_data data = ircam->GetData();
-        float to0 = ircam->GetRelativeTemperatureSensor0();
-        float to1 = ircam->GetRelativeTemperatureSensor1();
-        float ta = ircam->GetTemperatureInternal();
-        float ttrue = ircam->GetTrueTemperatureAveraged();
 
-        printf("data -> (ta: %i, to0: %i, to1 %i)\n", data.ta, data.t0, data.t1);
-        printf("to0 -> %.2f\n", to0);
-        printf("to1 -> %.2f\n", to1);
-        printf("ta -> %.2f\n", ta);
-        printf("ttrue -> %.2f\n", ttrue);
-        sleep_ms(500); 
-        clear_console();
-    }    
+    volatile MLX90614::config conf;
+
+    conf.value = ircam->ReadCTRLReg1();
+
+    printf("config: %i\n", conf.value);
+
     while(true)
     {
-        printf("dumpign ram:\n");
-        for(int i = 0; i < 0x1F; i++)
-        {
-            printf("%i -> %i\n", i, ircam->read_register(i));
-        }
-        printf("done\n");
-        sleep_ms(1000);
-        //clear_console();
+        // MLX90614::sensor_data data = ircam->GetData();
+        // //__breakpoint();
+        // float to0 = ircam->GetRelativeTemperatureSensor0();
+        // //__breakpoint();
+        // float to1 = ircam->GetRelativeTemperatureSensor1();
+        // float ta = ircam->GetTemperatureInternal();
+        // float ttrue = ircam->GetTrueTemperatureAveraged();
+        // float emiss = ircam->GetEmissivity();
+
+        // printf("emiss: %.3f\n", emiss);
+        // printf("data -> (ta: %i, to0: %i, to1 %i)\n", data.ta, data.t0, data.t1);
+        // printf("to0 -> %.2f\n", to0);
+        // printf("to1 -> %.2f\n", to1);
+        // printf("ta -> %.2f\n", ta);
+        // printf("ttrue -> %.2f\n", ttrue);
+        uint16_t v = ircam->ReadTO0();
+        float f = ircam->ConvertTO(v);
+        printf("%i 0x%x %.3f", v, v, f);
+        //float to0 = ircam->GetRelativeTemperatureSensor0();
+        sleep_ms(1500); 
+        clear_console();
     }
     while(true);
 }
