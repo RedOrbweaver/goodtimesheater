a402b1252fe3d330ceaa9746f22a01f0b74be789
RedOrbweaver <oliwier.ciesielski@proton.me>
2025-05-19 08:46:39 +0200

    working pwm

--- goodtimesheater.cpp
+++ goodtimesheater.cpp
@@ -18,14 +18,51 @@ int main()
 
     StartupLEDEffect();
     
-    ircam = std::make_unique<MLX90614>(I2C_PORT, I2C_SDA, I2C_SCL);
+    ircam = std::make_unique<MLX90614>(I2C_PORT, I2C_SDA, I2C_SCL, 50000, 0);
     sleep_ms(10);
 
-    volatile MLX90614::config conf;
+    ircam->SetMustSwitchFromPWM(true);
+    // uint16_t max = ircam->GetTOMax();
+    // uint16_t min = ircam->GetTOMin();
+    // printf("max: %i, min: %i\n", max, min);
 
-    conf.value = ircam->ReadCTRLReg1();
+    // volatile MLX90614::config conf;
+    // volatile MLX90614::pwmctrl pwm;
+    // conf.value = ircam->ReadCTRLReg1();
+    // pwm.value = ircam->ReadPWMCTRLReg();
 
-    printf("config: %i\n", conf.value);
+    // printf("config: 0x%x\n", conf.value);
+    // printf("pwmctrl: 0x%x\n", pwm.value);
+
+    ircam->StartReadingPWM(380.0f, 0.0f);
+
+    float val;
+    bool hasread = false;
+
+    ircam->SetOnPWMMeasurement([&](float v)
+    {
+        val = v;
+        hasread = true;
+    });
+
+    while(true)
+    {
+        if(hasread)
+        {
+            printf("Reading: %.3fC\n", val);
+            hasread = false;
+        }
+        sleep_ms(10);
+    }
+
+    init_in(I2C_SDA);
+    init_out(I2C_SCL, true);
+
+    while(true)
+    {
+        printf("%i\n", gpio_get(I2C_SDA));
+        sleep_ms(10);
+    }
 
     while(true)
     {
@@ -44,10 +81,28 @@ int main()
         // printf("to1 -> %.2f\n", to1);
         // printf("ta -> %.2f\n", ta);
         // printf("ttrue -> %.2f\n", ttrue);
-        uint16_t v = ircam->ReadTO0();
-        float f = ircam->ConvertTO(v);
-        printf("%i 0x%x %.3f", v, v, f);
-        //float to0 = ircam->GetRelativeTemperatureSensor0();
+        // conf.value = ircam->ReadCTRLReg1();
+        // pwm.value = ircam->ReadPWMCTRLReg();
+        // printf("config: 0x%x\n", conf.value);
+        // printf("pwmctrl: 0x%x\n", pwm.value);
+        // printf("max: %i, min: %i\n", ircam->GetTOMax(), ircam->GetTOMin());
+
+        // uint16_t t0 = ircam->ReadTO0();
+        // uint16_t t1 = ircam->ReadTO1();
+        // uint16_t ta = ircam->ReadTO1();
+        // float ft0 = ircam->ConvertTO(t0);
+        // float ft1 = ircam->ConvertTO(t1);
+        // float fta = ircam->ConvertTA(ta);
+        // //ircam->IncAddr();
+        // printf("t0: %i 0x%x %.3f\n", t0, t0, ft0);
+        // printf("t1: %i 0x%x %.3f\n", t1, t1, ft1);
+        // printf("ta: %i 0x%x %.3f\n", ta, ta, fta);
+        // printf("%i\n", ircam->GetAddress());
+        // if(t0 != 25)
+        // {
+        //     __breakpoint();
+        //     while(true);
+        // }
         sleep_ms(1500); 
         clear_console();
     }
